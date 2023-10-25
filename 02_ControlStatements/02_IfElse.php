30c2e4c61d21bbeb97e4feb8a830a24f2b06d5bd
prashant <prashanttalele1997@gmail.com>
2023-10-25 18:16:37 +0530

    commit

--- 02_ControlStatements/02_IfElse.php
+++ 02_ControlStatements/02_IfElse.php
@@ -18,15 +18,49 @@
                if($num1<$num2)
                
                  {
-                  echo"$num1 is  less than $num2";
+                  echo"$num1 is  less than $num2 </br>";
                  }
                  else
                  {
-                  echo"$num2 is Grater than $num1";
+                  echo"$num2 is Grater than $num1</br>";
                  }
                
-          ?>
+        ?>
+         
+         <?php
+            $num1=20;
+            $num2=30; 
+
+               if($num1>$num2)
+               
+                 {
+                  echo"$num1 is  less than $num2 </br>";
+                 }
+                  else
+                 {
+                  echo"$num2 is Grater than $num1</br>";
+                 }
+               
+            ?>
+
+             
+                 <?php
+
+                 $num=20;
+
+                if($num%2==0)
+                {
+                      echo"this is even Number </br>";
+                }
+                else
+                {
+                        echo"this odd  Number </br>";    
+                }  
+              ?>
+
+
+                 
     
-      
+         
 </body>
 </html>
