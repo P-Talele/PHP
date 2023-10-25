bb8f683f0b47e2fc790d6e96abd11d46770fa877
prashant <prashanttalele1997@gmail.com>
2023-10-25 18:00:59 +0530

    commit

---
+++ 02_ControlStatements/02_IfElse.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>IF Else</h1>
+
+
+    <h1>PHP If Statement</h1>
+       
+      <?php
+            $num1=20;
+            $num2=30; 
+
+               if($num1<$num2)
+               
+                 {
+                  echo"$num1 is  less than $num2";
+                 }
+                 else
+                 {
+                  echo"$num2 is Grater than $num1";
+                 }
+               
+          ?>
+    
+      
+</body>
+</html>
