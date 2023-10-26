e02116ef6a51dd310044c88721e44218345266a1
prashant <prashanttalele1997@gmail.com>
2023-10-26 21:34:23 +0530

    commit

---
+++ 04_Function/01_funtion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+      <h1> Function</h1>
+      <!-- PHP function is a piece of code that can be reused many times. It can take input as argument list and return value. 
+        There are thousands of built-in functions in PHP.-->
+
+          <?php
+               
+                function disp()
+                {
+                        echo"hi prashant how are you";
+                }
+
+                 disp(); // fucntion calling 
+          ?>
+</body>
+</html>
