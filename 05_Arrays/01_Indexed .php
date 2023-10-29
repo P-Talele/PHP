32ed901f446c2e41675d067bff869b6f8295cf24
prashant <prashanttalele1997@gmail.com>
2023-10-29 13:13:17 +0530

    commit

---
+++ 05_Arrays/01_Indexed .php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head> 
+<body>
+    <h1>Arrays</h1>>
+    <!---  PHP array is an ordered map (contains value on the basis of key).
+       It is used to hold multiple values of similar type in a single variable.---->  
+       
+       
+    <h2>PHP Indexed Array</h2>
+       <!-- PHP index is represented by number which starts from 0. We can store number
+       , string and object in the PHP array. All PHP array elements are assigned to an index number by default.--->
+ <?php
+    
+
+     $arr=Array("prashant","Arun ","Talele");
+     echo"$arr[0]";
+           
+  ?>
+  
+</body>
+</html>
