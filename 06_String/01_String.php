08f7904cd3455360a9eaab5c434980f1a9ba3ae0
prashant <prashanttalele1997@gmail.com>
2023-10-31 17:18:25 +0530

    commit

--- 06_String/01_String.php
+++ 06_String/01_String.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>PHP String</h2>  
-</body>
+     <h2>PHP String</h2>  
+
+       <?php  
+         $str='Hello text within single quote';  
+         echo $str;  
+       ?>  
+
+          <?php  
+          $str1='Hello text   
+          multiple line  
+          text within single quoted string';  
+          $str2='Using double "quote" directly inside single quoted string';  
+          $str3='Using escape sequences \n in single quoted string';  
+          echo "$str1 <br/> $str2 <br/> $str3";  
+         ?> 
+</body>  
 </html>
