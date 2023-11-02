e5c3c44da8f0b5025d86ca58214d9e9f4a001617
prashant <prashanttalele1997@gmail.com>
2023-11-02 17:42:25 +0530

    commit

--- Laravel/Laravel_F_Project/routes/web.php
+++ Laravel/Laravel_F_Project/routes/web.php
@@ -1,8 +1,9 @@
 <?php
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controlers\DemoController;
+use App\Http\Controllers\DemoController as ControllersDemoController;
 
-use Illuminate\Support\Facades\Route;
-
-/*
+ /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -10,9 +11,30 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+| 
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo',function()
+{
+    echo"hi prashant welcome to php";
+});  
+Route::get('/d',function()
+{
+  echo"hi prashant";
+});
+
+Route::get('name/{name}',function($name=null)
+  {
+      echo"name=$name";
+});
+ 
+//  Route::get('login/',function()
+//  {
+//     return view('login');  
+//  });
+
+Route::get('/login',[ControllersDemoController::class,'index']);
