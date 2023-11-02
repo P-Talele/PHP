aee50395d49550b3fbea1a68ae2e67bb6ba75595
prashant <prashanttalele1997@gmail.com>
2023-11-02 11:05:36 +0530

    commit

--- 06_String/01_String.php
+++ 06_String/01_String.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body>   
      <h2>PHP String</h2>  
-
-       <?php  
+  
+       <?php      
          $str='Hello text within single quote';  
          echo $str;  
        ?>  
