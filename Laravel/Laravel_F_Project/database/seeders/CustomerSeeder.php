b4fecc669bb41e0d5c1c928063cf62997250e91f
prashant <prashanttalele1997@gmail.com>
2023-11-10 14:03:26 +0530

    commit

---
+++ Laravel/Laravel_F_Project/database/seeders/CustomerSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use \App\Models\Customer;
+use \Faker\Factory  as Faker;   
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+         $faker=Faker::create();
+         for($i=1; $i<=500; $i++)
+       {
+        $customer  =new  Customer;
+        $customer->name=$faker->name;
+        $customer->gmail=$faker->email;
+        $customer->gender="M";
+        $customer->address=$faker->address;
+        $customer->dob=$faker->date;
+        $customer->password=$faker->password;
+        $customer->status="1";
+        $customer->save();
+       }    
+    }
+}
